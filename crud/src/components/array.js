f70d2ae26c2628b9caabb186c1f9c7878f596bd8
Krishna <mgkkratos@gmail.com>
2024-01-06 13:54:54 -0500

    CRUD operations on array used browsers localStorage

---
+++ crud/src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id : "1",
+        Name : "Krishna",
+        Age : "23",
+    },
+    {
+        id: "2",
+        Name: "Simran",
+        Age: "22",
+    },
+    {
+        id: "3",
+        Name: "Aakash",
+        Age: "28",
+    },
+];
+
+export default array;
